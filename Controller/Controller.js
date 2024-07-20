679d7c0fecafc6233a5bd49896b8a66c35e25c7a
mansigolaviyarajodiya <126144952+mansigolaviyarajodiya@users.noreply.github.com>
2024-07-20 10:51:43 +0530

    api created

--- Controller/Controller.js
+++ Controller/Controller.js
@@ -9,14 +9,12 @@ const getAllCategories = async (req, res) => {
         res.status(200).json({ data: categories, message: 'Categories fetched', success: true });
     } catch (error) {
         console.error('Error fetching categories:', error);
-        res.status(500).json({ message: 'Internal server error' });
+        res.status(500).json({success:false,message:error.message})
     }
 }
 
 
-
-
-const postCategories=async(req,res)=>{
+const postProducts=async(req,res)=>{
     try {
         const product = await Product.create(req.body)
         console.log('New user created:', product.toJSON());
@@ -24,10 +22,10 @@ const postCategories=async(req,res)=>{
         {
          return  res.json({success:false,message:"Not Insert",statusCode:400})
         }
-        res.json({success:true,message:"Insert",statusCode:200,data:product.toJSON()})
+        res.status(200).json({success:true,message:"Insert",data:product.toJSON()})
 
       } catch (error) {
-        res.json({success:false,message:error.message,statusCode:400})
+        res.status(500).json({success:false,message:error.message})
       }
 }
 
@@ -38,29 +36,109 @@ const postOrders=async(req,res)=>{
         console.log('New order created:', order.toJSON());
         if(!order)
         {
-         return  res.json({success:false,message:"Not Insert",statusCode:400})
+         return  res.status(404).json({success:false,message:"Not Insert"})
         }
         res.json({success:true,message:"Insert",statusCode:200,data:order.toJSON()})
 
       } catch (error) {
-        res.json({success:false,message:error.message,statusCode:400})
+        res.status(500).json({success:false,message:error.message})
       }
 }
 
-const getAllProducts = async (req, res) => {
+// with first sort by rank and then in 10 datas for pagination
+const getProductByRank = async (req, res) => {
     try {
-        const Product1 = await Product.findAll();
-        res.status(200).json({ data: Product1, message: 'Product fetched', success: true });
-    } catch (error) {
-        console.error('Error fetching categories:', error);
-        res.status(500).json({ message: 'Internal server error',success: false,error:error.message });
-    }
+        
+        const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
+        const pageSize = parseInt(req.query.pageSize, 10) || 10; 
+    
+        if (pageNumber < 1 || pageSize < 1) {
+          return res.status(400).json({ error: 'Invalid page number or page size' });
+        }
+    
+        const products = await Product.findAll({
+          order: [['rank', 'ASC']], 
+          limit: pageSize, 
+          offset: (pageNumber - 1) * pageSize, 
+        });
+    
+
+        if(products.length==0) return res.status(404).json({success:false,message:"Not Founded"})
+
+        res.status(200).json({success:true,message:"Products Founded",data:products})
+      } catch (error) {
+        console.error('Error fetching products:', error);
+         res.status(500).json({success:false,message:error.message})
+      }
+}
+
+// get product By productId
+const getProductById = async (req, res) => {
+    try {
+        const id = req.params.productId;
+        const products = await Product.findByPk(id);
+        if(!products) return res.status(404).json({success:false,message:"Not Founded"})
+        res.status(200).json({success:true,message:"Products Founded",data:products})
+      } catch (error) {
+        console.error('Error fetching products:', error);
+         res.status(500).json({success:false,message:error.message})
+      }
+}
+
+// delete product By productId
+const deleteProductById = async (req, res) => {
+    try {
+        const id = req.params.productId;
+        const products = await Product.destroy({where:{productId:id}});
+        if(!products) return res.status(404).json({success:false,message:"Not Founded"})
+        res.status(200).json({success:true,message:"Products Deleted",data:products})
+      } catch (error) {
+        console.error('Error fetching products:', error);
+         res.status(500).json({success:false,message:error.message})
+      }
 }
 
 
+const getCategoriesById=async(req,res)=>{
+    try {
+        const id=req.params.categoryId
+        const category = await Category.findByPk(id);
+
+        const pageNumber = parseInt(req.query.pageNumber, 10) || 1;
+        const pageSize = parseInt(req.query.pageSize, 10) || 10; 
+    
+        if (pageNumber < 1 || pageSize < 1) {
+          return res.status(400).json({ error: 'Invalid page number or page size' });
+        }
+        
+        if (!category) {
+            return res.status(404).json({success:false,message:"Category not found"})
+        }
+    
+        const products = await Product.findAll({
+          where: {
+            CategoryId: category.id
+          },
+          limit: pageSize, 
+          offset: (pageNumber - 1) * pageSize, 
+        });
+    
+        if (products.length==0) {
+            return res.status(404).json({success:false,message:"products not found"})
+        }
+        res.status(200).json({success:true,message:"Products Founded",data:products})
+      } catch (error) {
+        res.status(500).json({success:false,message:error.message})
+        console.error('Error fetching category and products:', error);
+      }
+  }
+
 module.exports = {
     getAllCategories,
-    postCategories,
-    getAllProducts,
-    postOrders
+    postProducts,
+    getProductByRank,
+    postOrders,
+    getProductById,
+    deleteProductById,
+    getCategoriesById
 }
--- Routes/Routers.js
+++ Routes/Routers.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const { getAllCategories,postCategories,getAllProducts,postOrders } = require('../Controller/Controller');
+const { getAllCategories,postProducts,getProductByRank,postOrders,getProductById,deleteProductById,getCategoriesById } = require('../Controller/Controller');
 const router = express.Router();
 
 // GET /categories - Retrieve all categories
 router.get('/categories', getAllCategories);
 
-router.post('/products',postCategories)
+router.post('/products',postProducts)
 
 router.post('/order',postOrders)
 
-router.get('/products', getAllProducts);
+router.get('/products', getProductByRank);
 
+router.get('/products/:productId',getProductById)
+
+router.delete('/products/:productId',deleteProductById)
+
+router.get('/products/:categoryId',getCategoriesById)
 
 
 module.exports = router;
